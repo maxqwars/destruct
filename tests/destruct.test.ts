de1d9676ede2de425b9eacef57f28988bc2e4f98
Maxim maxqwars Maximenko <maxqwars@gmail.com>
2024-07-29 21:31:16 +0700

    Added: First tests

---
+++ tests/destruct.test.ts
@@ -0,0 +1,55 @@
+import { destruct } from "../src";
+
+describe("ISO 639-1 language code format", () => {
+  test("should return an array of 'keywords' for an English string", () => {
+    const result = destruct(
+      "President Obama woke up Monday facing a Congressional defeat that many in both parties believed could hobble his presidency.",
+      {
+        language: "en",
+        returnChangedCase: true,
+      }
+    );
+
+    expect(result).not.toBeFalsy();
+    expect(result).toBe([
+      "president",
+      "obama",
+      "woke",
+      "monday",
+      "facing",
+      "congressional",
+      "defeat",
+      "parties",
+      "believed",
+      "hobble",
+      "presidency",
+    ]);
+  });
+
+  test("should return an array of 'keywords' for a Russian string", () => {
+    const result = destruct(
+      "Президент Обама проснулся понедельник перед Конгрессом поражение, что многие в обе стороны мнению, могли бы ковылять его президентства.",
+      {
+        language: "ru",
+        returnChangedCase: true,
+      }
+    );
+
+    expect(result).not.toBeFalsy();
+    expect(result).toBe([
+      "президент",
+      "обама",
+      "проснулся",
+      "понедельник",
+      "конгрессом",
+      "поражение",
+      "многие",
+      "обе",
+      "стороны",
+      "мнению",
+      "могли",
+      "ковылять",
+      "президентства",
+    ]);
+  });
+});
